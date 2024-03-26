eb128cc3309619e79d870cf418926fdae3b970f7
MicoBledsoe <micomods@gmail.com>
2024-03-26 16:06:39 -0500

    push work

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Explode : MonoBehaviour
+{
+    public VideoPlayer videoPlayer; 
+    public string nextSceneName = "NextScene"; 
+
+    void Start()
+    {
+        // Ensure the video doesn't play immediately
+        videoPlayer.playOnAwake = false;
+        videoPlayer.loopPointReached += EndReached; // Subscribe to the event
+    }
+
+    // Method to be called by the UI button to start video playback
+    public void PlayVideo()
+    {
+        videoPlayer.Play();
+    }
+
+    // Load the next scene after the video finishes playing
+    private void EndReached(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
